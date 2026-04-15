b8b320c412caf3d6e7731ce495c7244ff2fa951a
rohansingh-dev <rohansingh.techie@gmail.com>
2026-04-15 10:01:36 +0530

    update

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-$('.navbar-nav .nav-item').click(function(){
-    $('.navbar-nav .nav-link').removeClass('active');
-    $(this).addClass('active');
-})
-
-$(".alert").delay(4000).slideUp(200, function() {
-    $(this).alert('close');
-});
